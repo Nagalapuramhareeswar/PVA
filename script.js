14475163cce98b0df791a8fcaee33e40fc8a8a6d
NAGALAPURAM HAREESWAR <135661045+Nagalapuramhareeswar@users.noreply.github.com>
2025-12-30 13:18:03 +0530

    Implement form submission and disable right-click
    Added form submission handling with API integration and disabled right-click functionality.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Mobile Menu Interactivity
 const menuToggle = document.getElementById('mobile-menu');
 const navLinks = document.getElementById('navLinks');
+const dropbtn = document.getElementById('dropbtn');
+const dropdown = document.getElementById('dropdown-content');
 
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
@@ -20,24 +22,52 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     });
 });
 
-// Handle Form Submission
-document.getElementById('regForm').addEventListener('submit', function(e) {
+//disable right click
+document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
-    
-    // In a real app, you would send this data to a server
-    alert('Application Received! We will review your skill level and contact you via email.');
-    this.reset();
+    });
+
+
+// Handle Form Submission
+const form = document.getElementById('regForm');
+const submitBtn = document.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    formData.append("access_key","400eaf8d-b70f-4825-9b3d-37ad62b09aa0");
+    const originalText = submitBtn.textContent;
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+        const data = await response.json();
+        if (response.ok) {
+            window.location.href = "https://web3forms.com/success";
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
 });
 
-// // Scroll-based Header Styling
-// window.addEventListener('scroll', () => {
-//     const nav = document.querySelector('.navbar');
-//     if (window.scrollY > 50) {
-//         nav.style.background = '#020617'; // Darker on scroll
-//         nav.style.padding = '1rem 5%';
-//     } else {
-//         nav.style.background = '#0f172a';
-//         nav.style.padding = '1rem 5%';
-//     }
-
-// });
+// Scroll-based Header Styling
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        nav.style.background = '#020617'; // Darker on scroll
+        nav.style.padding = '0.7rem 5%';
+    } else {
+        nav.style.background = '#0f172a';
+        nav.style.padding = '1rem 5%';
+    }
+});
